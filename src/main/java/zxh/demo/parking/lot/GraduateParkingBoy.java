57c8a598fcbae68844da2bc254a7246370c8034b
zhangxuhai <xuhai.zhang@thoughtworks.com>
2019-11-19 11:57:59 +0800

    refactor: extract iterate parking lot method

--- src/main/java/zxh/demo/parking/lot/GraduateParkingBoy.java
+++ src/main/java/zxh/demo/parking/lot/GraduateParkingBoy.java
@@ -1,7 +1,9 @@
 package zxh.demo.parking.lot;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.BiFunction;
 
 /**
  * GraduateParkingBoy:
@@ -16,36 +18,37 @@ public class GraduateParkingBoy {
     }
 
     public Ticket park() {
-        Ticket result = null;
-        for (ParkingLot p : parkingLots) {
-            try {
-                result = p.park();
-                break;
-            } catch (ParkingLotFullException e) {
-                // do nothing
-            }
-        }
-
-        if (Objects.isNull(result)) {
-            throw new ParkingLotFullException();
-        }
-
-        return result;
+        return iterateParkingLotsToApply((p, t) -> p.park(), null, ParkingLotFullException.class);
     }
 
     public Ticket pick(Ticket ticket) {
+        return iterateParkingLotsToApply(ParkingLot::pick, ticket, InvalidTicketException.class);
+    }
+
+    private Ticket iterateParkingLotsToApply(BiFunction<ParkingLot, Ticket, Ticket> function, Ticket inputTicket, Class<? extends RuntimeException> exceptionType) {
+
         Ticket result = null;
         for (ParkingLot p : parkingLots) {
             try {
-                result = p.pick(ticket);
+                result = function.apply(p, inputTicket);
                 break;
-            } catch (InvalidTicketException e) {
+            } catch (Exception e) {
                 // do nothing
             }
         }
 
         if (Objects.isNull(result)) {
-            throw new InvalidTicketException();
+            RuntimeException exception;
+            try {
+                exception = exceptionType.getDeclaredConstructor().newInstance();
+            } catch (InstantiationException
+                    | IllegalAccessException
+                    | InvocationTargetException
+                    | NoSuchMethodException e) {
+                throw new IllegalArgumentException("Wrong Exception.");
+            }
+
+            throw exception;
         }
 
         return result;
