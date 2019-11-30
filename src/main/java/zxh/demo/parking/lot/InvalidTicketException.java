40b5274f0eeb3e32a351492da10b1d3d7acaf23a
zhangxuhai <xuhai.zhang@thoughtworks.com>
2019-11-30 14:23:42 +0800

    chore: reset to another time

--- src/main/java/zxh/demo/parking/lot/InvalidTicketException.java
+++
@@ -1,9 +0,0 @@
-package zxh.demo.parking.lot;
-
-/**
- * InvalidTicketException:
- * @author zhangxuhai
- * @date 2019/11/18
-*/
-public class InvalidTicketException extends RuntimeException {
-}
--- src/main/java/zxh/demo/parking/lot/ParkingLotFullException.java
+++
@@ -1,9 +0,0 @@
-package zxh.demo.parking.lot;
-
-/**
- * ParkingLotFullException:
- * @author zhangxuhai
- * @date 2019/11/18
-*/
-public class ParkingLotFullException extends RuntimeException {
-}
